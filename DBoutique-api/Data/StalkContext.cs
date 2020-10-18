26ca06465d3d60174501f1cfd854a68c63d2c1b5
nanfuka <kalungi2k6@gmail.com>
2020-10-18 04:41:48 +0300

    Transfer the api to DBoutique-API folder to create space for tests

---
+++ DBoutique-api/Data/StalkContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class StalkContext : DbContext
+    {
+        public StalkContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Stalk> Stolk { get; set; }
+
+    }
+}
---
+++ DBoutique-api/Models/stalk.cs
@@ -0,0 +1,16 @@
+namespace Models
+{
+    /**
+     * this stalk class include attributes of the shopped clothes
+     * Attributes are id, which is the primary key
+     * Type of item added to the stalk such as leggings and tops
+     * the cost of purchasing the item and the description
+    */
+    public class Stalk
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; }
+    }
+}
